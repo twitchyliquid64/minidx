4cd770181ecaa477acc0740b0aab17a70ccfa231
Tom <twitchyliquid64@ciphersink.net>
2025-03-23 21:42:29 -0700

    fix MNIST example/test

--- crates/minidx/tests/mnist.rs
+++ crates/minidx/tests/mnist.rs
@@ -6,14 +6,13 @@ use std::env;
 use std::fs::File;
 
 // RUST_MIN_STACK=104857600 cargo test --release -- --nocapture --include-ignored mnist_network
-// Still working on making this accurate lol
 
 #[test]
 #[ignore]
 fn mnist_network() {
     rayon::ThreadPoolBuilder::new()
         .num_threads(4)
-        .stack_size(128 * 1024 * 1024)
+        .stack_size(196 * 1024 * 1024)
         .build_global()
         .unwrap();
 
@@ -37,15 +36,16 @@ fn mnist_network() {
 
     let network = (
         (
-            layers::GLU::<INPUT_DIMS, 120>::default(),
-            layers::Swish::<120> {},
-            layers::DyT::<120> {},
+            layers::Linear::<INPUT_DIMS, 140>::default(),
+            layers::Swish::<140> {},
+            layers::DyT::<140> {},
         ),
         (
-            layers::Linear::<120, 40>::default(),
-            layers::Swish::<40> {},
-            layers::DyT::<40> {},
+            layers::Linear::<140, 80>::default(),
+            layers::Swish::<80> {},
+            layers::DyT::<80> {},
         ),
+        (layers::GLU::<80, 40>::default(), layers::Swish::<40> {}),
         layers::Linear::<40, 10>::default(),
         layers::Softmax::default(),
     );
@@ -57,26 +57,25 @@ fn mnist_network() {
 
     use minidx_core::loss::LogitLoss;
     let mut updater = nn.new_rmsprop_with_momentum(
-        TrainParams::with_lr(7.5e-3)
-            .and_l2(1.5e-5)
-            .and_soft_start(150)
-            .and_lr_cosine_decay(2.0e-4, 42000),
+        TrainParams::with_lr(4.0e-3)
+            .and_l2(0.5e-6)
+            .and_soft_start(100)
+            .and_lr_cosine_decay(1.0e-3, 40000),
         0.8,
-        0.99,
+        0.97,
     );
-    for i in 0..45000 {
+    for i in 0..70000 {
         let avg_loss = train_batch_parallel(
             &mut updater,
             &mut nn,
             |got, want| (got.logit_bce(want), got.logit_bce_input_grads(want)),
             &mut || p.sample(),
             match i {
-                0..1000 => 64,
-                1000..4000 => 32,
-                4000..6000 => 20,
-                6000..10000 => 16,
-                10000..15000 => 12,
-                _ => 10,
+                0..2000 => 64,
+                2000..6000 => 48,
+                6000..15000 => 32,
+                15000..30000 => 24,
+                _ => 20,
             },
         );
         if i % 20 == 0 {
@@ -89,11 +88,11 @@ fn mnist_network() {
         }
     }
 
-    for _ in 0..30 {
+    for _ in 0..15 {
         let (input, target) = p.sample();
         let out = nn.forward(&input).unwrap();
         let loss = out.logit_bce(&target);
         println!("got={:?}, want={:?}: loss={}", out, target, loss);
-        assert!(loss < 0.25);
+        assert!(loss < 0.1);
     }
 }
